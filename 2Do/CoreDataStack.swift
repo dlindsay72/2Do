1f9238c2876495f95804b0af486a24d0bd034cc8
Dan Lindsay <dlindsay72@hotmail.com>
2017-09-08 14:44:00 -0400

    refactored CoreDataStack, added persistentContainer and managedObjectContext

--- 2Do/CoreDataStack.swift
+++ 2Do/CoreDataStack.swift
@@ -11,18 +11,70 @@ import CoreData
 
 class CoreDataStack {
     
-    private(set) lazy var applicationDocumentsDirectory: URL = {
-        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        
-        let endIndex = urls.index(before: urls.endIndex)
-        
-        return urls[endIndex]
+//    private(set) lazy var applicationDocumentsDirectory: URL = {
+//        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//        let endIndex = urls.index(before: urls.endIndex)
+//        
+//        return urls[endIndex]
+//    }()
+//    
+//    
+//    private(set) lazy var managedObjectModel: NSManagedObjectModel = {
+//        let modelUrl = Bundle.main.url(forResource: "2Do", withExtension: "momd")!
+//        
+//        return NSManagedObjectModel(contentsOf: modelUrl)!
+//    }()
+//    
+//    private(set) lazy var persistantStoreCoordinator: NSPersistentStoreCoordinator = {
+//        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+//        let url = self.applicationDocumentsDirectory.appendingPathComponent("2Do.sqlite")
+//        
+//        do {
+//            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url)
+//        } catch {
+//            print(error)
+//            abort()
+//        }
+//        
+//        return coordinator
+//    }()
+    
+    lazy var managedObjectContext: NSManagedObjectContext = {
+        let container = self.persistentContainer
+        return container.viewContext
     }()
     
-    
-    private(set) lazy var managedObjectModel: NSManagedObjectModel = {
-        let modelUrl = Bundle.main.url(forResource: "2Do", withExtension: "momd")!
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "2Do")
+        container.loadPersistentStores() { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error: \(error), \(error.userInfo)")
+            }
+        }
         
-        return NSManagedObjectModel(contentsOf: modelUrl)!
+        return container
     }()
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
