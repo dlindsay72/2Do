80aeab4cb10c9c2ca633667f0d29c6788972c8e9
Dan Lindsay <dlindsay72@hotmail.com>
2017-09-11 14:24:03 -0400

    create and perform fetch request. CoreData is saving data

--- Item+CoreDataProperties.swift
+++ Item+CoreDataProperties.swift
@@ -13,7 +13,11 @@ import CoreData
 extension Item {
 
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Item> {
-        return NSFetchRequest<Item>(entityName: "Item")
+        let request =  NSFetchRequest<Item>(entityName: "Item")
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "text", ascending: true)]
+        
+        return request
     }
 
     @NSManaged public var isCompleted: Bool
